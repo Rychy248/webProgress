e37cd2ea18cfd805d0852480eee5742243865a6e
Rychy248 <jorgeajrhh@gmail.com>
2023-03-18 10:00:36 -0600

    Sesion 11 started

--- Sesion 11 The Document Object Model/static/js/index.js
+++ Sesion 11 The Document Object Model/static/js/index.js
@@ -3,3 +3,12 @@
 function hiExternalJS(){
     alert("Hello from Index.js, \n\n An external JS file!")
 };
+
+function changeMyTtile(){
+    let container = document.getElementById("DOM-box");
+
+    console.log("Im the 'The first Child' container.firstElementChild " + container.firstElementChild);
+    alert("Im the 'The first Child' container.firstElementChild " + JSON.stringify(container.firstElementChild));
+
+    container.firstElementChild.innerHTML = "I'm modified succesfull";
+};

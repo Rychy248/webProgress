929e0a8d39e228009fd6e896ef3170c93248aa24
Rychy248 <jorgeajrhh@gmail.com>
2023-03-29 00:16:59 -0600

    sounds linked

--- Sesion 13 Advanced JavaScript and DOM/static/js/index.js
+++ Sesion 13 Advanced JavaScript and DOM/static/js/index.js
@@ -2,9 +2,25 @@
 document.querySelectorAll(".drum").forEach(drum => {
     drum.addEventListener("click", 
         function () {
-            let my_audio = new Audio('/Sesion 13 Advanced JavaScript and DOM/static/sounds/tom-1.mp3');
-            my_audio.play();
+
+            this.classList.add("sound"); // adding higlith the button
+            
+            let my_audio = new Audio(
+                "/Sesion 13 Advanced JavaScript and DOM/static/sounds/" + 
+                (
+                    (this.textContent === "w") ? "tom-1" :
+                    (this.textContent === "a") ? "tom-2" :
+                    (this.textContent === "s") ? "tom-3" :
+                    (this.textContent === "d") ? "tom-4" :
+                    (this.textContent === "j") ? "snare" :
+                    (this.textContent === "k") ? "crash" :
+                    "kick-bass" 
+                ) + ".mp3"
+            );
+
+            my_audio.play(); //play the sound
+            setTimeout(() => { this.classList.remove("sound"); }, 200); // removing the higlith of the button
         }
     );
-}); 
 
+}); 

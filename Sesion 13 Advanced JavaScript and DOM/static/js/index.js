b7be7dccc524b264fc2653ed6c2790f3e66ea6c7
Rychy248 <jorgeajrhh@gmail.com>
2023-03-29 18:58:46 -0600

    Sesion 13 finished

--- Sesion 13 Advanced JavaScript and DOM/static/js/index.js
+++ Sesion 13 Advanced JavaScript and DOM/static/js/index.js
@@ -4,14 +4,27 @@ document.querySelectorAll(".drum").forEach(drum => {
     drum.addEventListener("click", 
         function () {
 
+            this.classList.add("pressed"); // adding higlith the button
             this.classList.add("sound"); // adding higlith the button
             
             let my_audio = createAudio(this.textContent)
-
             my_audio.play(); //play the sound
 
-            setTimeout(() => { this.classList.remove("sound"); }, 200); // removing the higlith of the button
+            setTimeout(() => { 
+                this.classList.remove("pressed");
+                this.classList.remove("sound");
+            }, 200); // removing the higlith of the button
         }
     );
 
 }); 
+
+function clickButton(btnClass) {
+    if (["w","a","s","d","j","k","l"].includes(btnClass)) {
+        document.querySelector(`.drum.${btnClass}`).click();
+    };
+};
+
+document.addEventListener("keydown",(event)=>{
+    clickButton(event.key.toLowerCase());
+});

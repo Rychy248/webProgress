29db31e4e2ad4afb06e65e2bc1a32555b24cfd8a
Rychy248 <jorgeajrhh@gmail.com>
2023-05-07 00:07:02 -0600

    fs module of nodejs

--- Sesion 17 & 18: Back-end & NodeJs/index.js
+++ Sesion 17 & 18: Back-end & NodeJs/index.js
@@ -5,11 +5,3 @@ let containers = document.querySelectorAll(".container");
 containers[containers.length -1].style.paddingBottom = "2rem";
 
 
-
-
-
-
-
-
-
-const fs = require("fs");
--- Sesion 17 & 18: Back-end & NodeJs/node/index.js
+++ Sesion 17 & 18: Back-end & NodeJs/node/index.js
@@ -1,1 +1,9 @@
+
+// File Script, excuted from Node, into the terminal
 console.log("Hello World");
+
+
+
+const fs = require("fs");
+
+fs.copyFileSync("./file1.txt","./Copy of file1.txt")

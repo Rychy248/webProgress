dc85e61e9109df53b007c8888d056d71ec7c1be0
Rychy248 <jorgeajrhh@gmail.com>
2023-05-16 01:36:48 -0600

    express reder a file

--- Session 24 Express/calculator/main.js
+++ Session 24 Express/calculator/main.js
@@ -1,152 +1,29 @@
 
 const express = require("express");
-
+const path = require("path");
 const myApp = express();
 const port = 8080;
 
-const style = `
-    <style>
-        /* ---------------------GENERAL--------------------- */
-        :root{
-        --border-radius: 30px;
-        --border-weight: 2px;
-
-        --ubuntu: 'Ubuntu', sans-serif;
-        --montserrat: 'Montserrat', sans-serif;
-        --creepster: 'Creepster', cursive;
-        --code: 'M PLUS 1 Code', sans-serif;
-
-        --principal-color:#D9EEEC;
-        --secondary-color:#3C70A4;
-        --third-color:#64B2CD;
-        --forth-color:#DA9833;
-        --fifth-color:#F65C78;
-
-        --font-size-l: 2.3rem;
-        --font-size-m: 1.8rem;
-        --font-size-s: 1.3rem;
-        }
-
-        *{
-        margin: 0;
-        padding: 0;
-        border: 0;
-        font-family: var(--montserrat);
-        font-size: var(--font-size-s);
-        }
-        body{
-        text-align: center;
-        background-color: var(--principal-color);
-        }
-
-        body{
-            width: 90%;
-            margin: 0 auto;
-        }
-        /* ----------------- HEADERS */
-        h1, h2, h3, h4, h5, h6{
-        font-family: var(--ubuntu);
-        color: var(--third-color);
-        font-weight: bolder;
-        }
-        h1 > span, h2 > span, h3 > span, h4 > span, h5 > span, h6 > span{
-        font-family: var(--ubuntu);
-        font-size: var(--font-size-m);
-        }
-        h1, h2{
-        font-size: var(--font-size-l);
-        text-align: center;
-        }
-
-        h1 > a, h2 > a, h3 > a{
-        color: var(--third-color);
-        font-family: var(--ubuntu);
-        font-size: var(--font-size-m);
-        }
-        /* ----------------- PARAGRAHP AND LISTS */
-        p{
-        text-align: left;
-        }
-
-        .resalt{
-            color: var(--forth-color);
-        }
-
-        /* ----------------- BUTTON */
-        button{
-        padding: 15px;
-        border-radius: var(--border-radius);
-        border: var(--border-weight) var(--secondary-color) double;
-
-        background-color: var(--white-color);
-
-        font-weight: bol;
-        color: var(--secondary-color);
-        }
-        button:hover, button a:hover{
-        background-color: var(--secondary-color);
-        color: var(--white-color);
-        border-color: var(--white-color);
-        }
-        button a{
-        text-decoration: none;
-        }
-        button a:visited{
-        color: var(--white);
-        }
-        @media (max-width: 1028px) {
-        h1, h2{
-        font-size: 2rem;
-        text-align: center;
-        }
-        p, li{
-        font-size:1rem;
-        }
-
-        button{
-        padding: 10px;
-        font-size: 1rem;
-        }
-
-        }
-    </style>
-`
 
 myApp.get("/",(req,res)=>{
-    res.send(
-        `
-        <h1 class="resalt">USING GET METHOD</h1>
-        <h1>Calculator</h1>
+    let options = {
+        // root: path.join(__dirname, 'public'), method join, unify the root from __dirname with the suposed public
+        root: __dirname,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
 
-        ${style}
-        <form action="calculate" method="GET">
-            <label for="a">a</label>
-            <input type="number" name="a">
-            <label for="a">b</label>
-            <input type="number" name="b"s>
-            <br>
-        
-            <input type="radio" name="operation" value="multiply" id="multiply" >
-            <label for="multiply">multiply</label>
-            
-            <input type="radio" name="operation" value="sum" id="sum">
-            <label for="sum">sum</label>
-            
-            <input type="radio" name="operation" value="divition" id="divition">
-            <label for="divition">divition</label>
-            
-            <input type="radio" name="operation" value="rest" id="rest">
-            <label for="rest">rest</label>
+    res.sendFile("index.html", options, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Sent:', "index.html");
+        };
+    });
 
-            <br>
-            <button type="submit">Calculate</button>
-        </form>
-
-        <h1 class="resalt">USING POST METHOD</h1>
-        <h1>Calculator</h1>
-
-        `
-    );
 });
 
 myApp.get("/calculate",(req,res)=>{
@@ -177,7 +54,7 @@ myApp.get("/calculate",(req,res)=>{
     };
 
     res.send(
-        ` ${style}
+        `
         <h1>Result</h1>
         <br>
         <p>The ${(operation !== undefined)? operation : "sum"} result is: ${result}</p>

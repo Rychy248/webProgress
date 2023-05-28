6553d0feb46147da2a99aa4984d17f043a63cd66
Rychy248 <jorgeajrhh@gmail.com>
2023-05-27 19:40:59 -0600

    Sesion 25 finished

--- Session 25 APIs/Newsletter/app.js
+++ Session 25 APIs/Newsletter/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { send } = require("process");
+const { stringify } = require("querystring");
 
 const port = 3000;
 let app = express();
@@ -17,47 +18,89 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({extended:true}));
 
 
-
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/signup.html")
 });
 
 app.post("/",(req,res)=>{
-    console.log(req.body)
     let name = req.body.name;
     let lastName = req.body['last-name'];
     let email = req.body.email;
+    let birthday = req.body.birthday;
+    birthday = `${birthday.split("-")[1]}/${birthday.split("-")[2]}`
 
-    res.write(`Name: ${name} `);
-    res.write(`Last Name: ${lastName} `);
-    res.write(`email: ${email} `);
-    res.send();
-});
-
-app.get("/test",(req,res)=>{
-
-
+    const apiKeys = require("./apiKeys");
     const mailchimp = require("@mailchimp/mailchimp_marketing");
-    const mailApiKeys = require("./apiKeys");
 
-    let response;
 
     mailchimp.setConfig({
-        apiKey: mailApiKeys.apiKey,
-        server: mailApiKeys.server,
+        apiKey: apiKeys.mailChimp.apiKey,
+        server: apiKeys.mailChimp.server,
     });
-    
+
     async function run() {
-        response = await mailchimp.ping.get();
-        console.log(response);
-    }
+        console.log("API called....");
     
-    run();
-    send(response);
+        const response = await mailchimp.lists.addListMember(`${apiKeys.mailChimp.listId}`,{
+            email_address: `${email}`,
+            status: `subscribed`,
+            merge_fields:{
+                FNAME: `${name}`,
+                LNAME: `${lastName}`,
+                BIRTHDAY: `${birthday}`
+            }
+        });
+        console.log("API response: "+response.status);
+        console.log(`Name: ${name} `);
+        console.log(`Last Name: ${lastName} `);
+        console.log(`email: ${email} `);
+        console.log(`birthday: ${birthday} `);
+
+        res.sendFile(__dirname + "/success.html",(err=>{err ? console.log(err) : console.log("success.html served...")}));
+    };
+
+    run().catch((err)=>{
+        res.sendFile(__dirname + "/failure.html",(err=>{err ? console.log(err) : console.log("Failre.html served...")}));
+    });
+
+
 
 });
+
+app.get("/getLists",(req,res)=>{
+
+    const apiKeys = require("./apiKeys");
+    const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+    mailchimp.setConfig({
+        apiKey: apiKeys.mailChimp.apiKey,
+        server: apiKeys.mailChimp.server,
+    });
+
+    const run = async () => {
+        const response = await mailchimp.lists.addListMember(`${apiKeys.mailChimp.listId}`,{
+            email_address: "example@gmail.com",
+            status: "subscribed",
+            FNAME:"",
+            LNAME:"",
+            PHONE:"",
+            BIRTHDAY:""
+        });
+        console.log(response);
+        res.write("api response: "+ JSON.stringify(response));
+        res.send();
+    };
+
+    run().catch((err)=>{
+        console.log("Error getted: "+err);
+    });
+
+
+
+});
+
+
 
 app.listen(port,()=>{
     console.log("App served port: http://127.0.0.1:"+port)
 });
-

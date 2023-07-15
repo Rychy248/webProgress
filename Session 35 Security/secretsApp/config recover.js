153f41cf8da88c1ca62103e49024cdc2f1a470d4
Rychy248 <jorgeajrhh@gmail.com>
2023-07-14 23:28:58 -0600

    config recover

--- Session 35 Security/secretsApp/config recover.js
+++ Session 35 Security/secretsApp/config recover.js
@@ -6,7 +6,6 @@ const
 // Third part modules 
     express = require("express"),
     ejs = require("ejs")
-    // cors = require('cors'),
     bodyParser = require('body-parser'),
     jsonParser = bodyParser.json(),
     urlEncoded = bodyParser.urlencoded({extended:true}),
@@ -20,17 +19,9 @@ const
     },
 // create app
     app = express(),
-// Allowed origin for cors util
-    // allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:8080/"];
-
 // -------- MIDLEWARE
 app.set('view engine', 'ejs');
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
-    // cors midleware
-// app.use(cors({origin: allowedOrigins}));
-// MODIFY THE JSON PARSER
-// app.use(bodyParser.urlencoded({extended:true}));
-
 
 module.exports = { express, app, dbSettings, appSettings, urlEncoded, jsonParser}

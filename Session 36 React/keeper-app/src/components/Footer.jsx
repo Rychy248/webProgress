799b412cfa6a0cbde7cd42ab613524224c663ce0
Rychy248 <jorgeajrhh@gmail.com>
2023-08-13 08:44:32 -0600

    Keeper App, challengue 1, finished

---
+++ Session 36 React/keeper-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return (
+        <footer>
+            <p>copyrigth &#169; 2018 - {new Date().getFullYear()}</p>
+        </footer>
+    );
+};
---
+++ Session 36 React/keeper-app/src/components/Note.jsx
@@ -0,0 +1,9 @@
+
+export default function Note(){
+    return(
+        <div className="note">
+            <h1>Hello</h1>
+            <p>From this note</p>
+        </div>
+    );
+};

386b52184a6877d88d4a1703c5f8b6ac6af241bc
Rychy248 <jorgeajrhh@gmail.com>
2023-08-18 22:39:28 -0600

    props started

---
+++ Session 36 React/viteProject/src/componentes/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card"
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <Card />
+    </>
+  )
+};
---
+++ Session 36 React/viteProject/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './componentes/App.jsx'
+
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

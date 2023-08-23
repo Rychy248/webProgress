5f1f59275b3dd051e95dcccd9ace359e17420bb8
Rychy248 <jorgeajrhh@gmail.com>
2023-08-22 23:59:35 -0600

    React propsPractice, finished

---
+++ Session 36 React/propsPractice/src/components/App.jsx
@@ -0,0 +1,18 @@
+// css style
+import './App.css'
+// components
+import Card from './Card';
+// data
+import contacts from './contacts';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map((c) => { return <Card key={c.email} name={c.name} imgSrc={c.imgURL} phone={c.phone} email={c.email} /> })}
+    </div>
+  );
+}
+
+export default App;
